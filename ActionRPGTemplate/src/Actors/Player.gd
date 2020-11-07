af85e2476be3280127914b2ff3ca0a3618bd7e6b
CCLA-Doane <61520801+CCLA-Doane@users.noreply.github.com>
2020-11-07 11:46:09 -0600

    7 November Lecture Update:
     Added Bushes and Trees, Rolling, and Terrain tiles.

--- ActionRPGTemplate/src/Actors/Player.gd
+++ ActionRPGTemplate/src/Actors/Player.gd
@@ -1,17 +1,15 @@
-extends KinematicBody2D
+extends KinematicBody2D # Everything KinematicBody2D can do, we can too
 
-# constant variables Acceleration, Friction, and Velocity
+# Constant variables
 const ACCELERATION = 500.0
 const FRICTION = 500.0
 var velocity = Vector2.ZERO
 
-# Animation State variables
-enum{
+enum {
 	MOVE,
 	ROLL,
 	ATTACK
 }
-
 # Set the default state
 var state = MOVE
 
@@ -25,32 +23,24 @@ onready var animation_state = animation_tree.get("parameters/playback")
 
 # _physics_process(delta) called every frame, reads player input, etc.
 func _physics_process(delta):
+	# Set up match statement for state machine
 	
-	# Set up a match (Godot switch) for the state machine
-	match state:
-		
-		# Move state
-		MOVE:
+	match state: 
+		MOVE: 
 			move_state(delta)
-		
-		# Roll state
-		ROLL:
-			pass
-		
-		# Attack
-		ATTACK:
+		ROLL: 
+			roll_state(delta)
+		ATTACK: 
 			attack_state(delta)
-
-# _ready(): this is called when the object is ready to be used in game
+	
 func _ready():
-	# Set active animation tree
 	animation_tree.active = true
 	
-	
-# Set up input, also move state
+# Set up input
 func move_state(delta: float) -> void:
 	# Create an input_vector variable initialize to 0, 0
 	var input_vector = Vector2.ZERO
+	
 	# Get input vectors on x and y by subtracting left from right and top from bottom
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -65,34 +55,39 @@ func move_state(delta: float) -> void:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
 		animation_tree.set("parameters/Attack/blend_position", input_vector)
-		
+		animation_tree.set("parameters/Roll/blend_position", input_vector)
 		# Move, finally, using animation
 		animation_state.travel("Run")
-		
-		# Set movement velocity
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, FRICTION * delta)
-	
+		velocity = velocity.move_toward(input_vector * MAX_SPEED,  ACCELERATION * delta)
+			# set roll state
+		if Input.is_action_just_pressed("ui_accept"):
+			state = ROLL
 	# Not moving?
 	else:
-		# Use idle animation
+		# Use idle animation		
 		animation_state.travel("Idle")
-		
-		# Use velocity variable to move the sprite on screen
+		# Use velocity variable to move the sprite on screen		
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
+	# Move the character on screen
 	move_and_slide(velocity)
-	
-	# Did we just press attack?
-	if Input.is_action_just_pressed("Attack"):
+	# Did we just press attack?	
+	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
-	
+		
+
 # Attack State Function
 func attack_state(delta: float) -> void:
 	velocity = Vector2.ZERO
 	animation_state.travel("Attack")
 
-# Stop playing attack animations
+# Roll state animation
+func roll_state(delta: float) -> void:
+	animation_state.travel("Roll")
+
+# Stop playing roll
+func roll_animation_finished():
+	state = MOVE
+
+# Stop playing attack
 func attack_animation_finished():
 	state = MOVE
-	
-
